fffe96c2a100277e7970a09287a5f6aaf3fa562f
srushti8 <76839929+srushti8@users.noreply.github.com>
2021-02-24 17:03:56 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,23 @@
+class Paper{
+    constructor(x, y, width, height) {
+        var options = {
+            isStatic:false;
+            'restitution':0.3,
+            'friction':0.5,
+            'density':1.2
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("paper.png");
+        World.add(world, this.body);
+      }
+      display(){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+      }
+}
